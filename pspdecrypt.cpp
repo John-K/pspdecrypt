5e883823caad88a9c9e9520d82b00a1604a3bd38
Arthur Blot <arthur.blot78@gmail.com>
2021-02-23 19:55:34 +0100

    Merge pspdecrypt & psardecrypt, and add a few basic options

--- pspdecrypt.cpp
+++ pspdecrypt.cpp
@@ -4,71 +4,157 @@
 #include <fstream>
 #include <string>
 #include <string.h>
+#include <getopt.h>
+#include "PsarDecrypter.h"
 #include "PrxDecrypter.h"
+#include "common.h"
 
 using namespace std;
-static const u32 ELF_SIGNATURE = 0x464C457F;
-static const u32 PSP_SIGNATURE = 0x5053507E;
-static const u32 PBP_SIGNATURE = 0x52415350;
+static const u32 ELF_MAGIC  = 0x464C457F;
+static const u32 PSP_MAGIC  = 0x5053507E;
+static const u32 PSAR_MAGIC = 0x52415350;
+static const u32 PBP_MAGIC  = 0x50425000;
 
-int
-main(int argc, char *argv[]) {
-	int outSize;
+void help(const char* exeName) {
+    cout << "Usage: " << exeName << " [OPTION]... [FILE]" << endl;
+    cout << endl;
+    cout << "Decrypts encrypted PSP binaries (.PSP) or updaters (.PSAR)." << endl;
+    cout << "Can also take PBP files as an input, or IPL binaries if the option is given." << endl;
+    cout << endl;
+    cout << "General options:" << endl;
+    cout << "  -v, --verbose      enable verbose mode (mostly for debugging)" << endl;
+    cout << "PSP(/PBP)-only options:" << endl;
+    cout << "  -o, --outfile      output file for the decrypted binary (default: [FILE.PSP].dec)" << endl;
+    cout << "PSAR(/PBP)-only options:" << endl;
+    cout << "  -e, --extract-only do not decrypt files contained in the PSAR" << endl;
+    cout << "IPL decryption & PSAR(/PBP) options:" << endl;
+    cout << "  -O, --outdir       output path for the PSAR's or IPL's contents (default: current directory)" << endl;
+    cout << "  -i, --ipl-decrypt  decrypt the IPL given as an argument" << endl;
+    cout << "  -p, --preipl       preipl image used for decrypting the later IPL stages" << endl;
+}
 
-	if (argc != 2) {
-		printf("Usage: %s <infile>\n", basename(argv[0]));
-		return 1;
-	}
-	string filename = string(argv[1]) + ".dec";
-	const char *outFilename = filename.c_str();
+int main(int argc, char *argv[]) {
 
-	ifstream inFile (argv[1], ios::in|ios::binary|ios::ate);
-	if (!inFile.is_open()) {
-		printf("Could not open '%s'\n", argv[1]);
-		return 1;
-	}
+    static struct option long_options[] = {
+        {"help",         no_argument,       0, 'h'},
+        {"verbose",      no_argument,       0, 'v'},
+        {"outfile",      required_argument, 0, 'o'},
+        {"extract-only", no_argument,       0, 'e'},
+        {"outdir",       required_argument, 0, 'O'},
+        {"ipl-decrypt",  no_argument,       0, 'i'},
+        {"preipl",       required_argument, 0, 'p'},
+        {0,              0,                 0,  0 }
+    };
+    int long_index;
 
-	streampos size = inFile.tellg();
-	char* inData = new char[size];
-	char* outData = new char[size];
-	inFile.seekg(0, ios::beg);
-	inFile.read(inData, size);
-	inFile.close();
+    string inFilename = "";
+    string outDir = ".";
+    string outFile = "";
+    string preipl = "";
+    bool verbose = false;
+    bool extractOnly = false;
+    bool iplDecrypt = false;
+    int c = 0;
+    while ((c = getopt_long(argc, argv, "hvo:eO:ip:", long_options, &long_index)) != -1) {
+        switch (c) {
+            case 'h':
+                help(argv[0]);
+                return 0;
+            case 'v':
+                verbose = true;
+                break;
+            case 'o':
+                outFile = string(optarg);
+                break;
+            case 'e':
+                extractOnly = true;
+                break;
+            case 'O':
+                outDir = string(optarg);
+                break;
+            case 'i':
+                iplDecrypt = true;
+                break;
+            case 'p':
+                preipl = string(optarg);
+                break;
+            default:
+                help(argv[0]);
+                return 1;
+        }
+    }
 
-	// detect what type of file we're dealing with
-	u32 file_signature = *(u32 *)inData;
-	switch (file_signature) {
-		case ELF_SIGNATURE:
-			printf("File is already decrypted, exiting.\n");
-			// let's write the file out as if it decrypted successfully to make things easier for folks
-			outSize = size;
-			goto write_file;
-		case 0:
-			printf("Found NULL file signature - is the file empty?\n");
-			return -1;
-		case PSP_SIGNATURE:
-			break;
-		case PBP_SIGNATURE:
-			printf("Found PBP, please run unpack-pbp. Exiting.\n");
-			return -1;
-		default:
-			printf("Found unknown file signature 0x%08X, exiting.\n", file_signature);
-			return -1;
-	};
+    if (optind >= argc) {
+        cerr << "No file specified!" << endl;
+        return 1;
+    }
 
-	outSize = pspDecryptPRX((const u8 *)inData, (u8 *)outData, size, nullptr, true);
-write_file:
-	printf("Decrypt returned %d\n", outSize);
-	if (outSize > 0) {
-		ofstream outFile;
-		outFile.open(outFilename, ios::out | ios::app | ios::binary);
-		if (!outFile.is_open()) {
-			printf("Could not open '%s' for output\n", outFilename);
-			return 1;
-		}
-		outFile.write(outData, outSize);
-		outFile.close();
-		return 0;
-	}
-	return outSize;
+    if (optind + 1 < argc) {
+        cerr << "More than two input files specified!" << endl;
+        return 1;
+    }
+
+    inFilename = string(argv[optind]);
+
+    if (outFile == "") {
+        outFile = inFilename + ".dec";
+    }
+
+    ifstream inFile (inFilename, ios::in|ios::binary|ios::ate);
+    if (!inFile.is_open()) {
+        cerr << "Could not open " << inFilename << "!" << endl;
+        return 1;
+    }
+
+    streampos size = inFile.tellg();
+    char* inData = new char[size];
+    char* outData = new char[size];
+    inFile.seekg(0, ios::beg);
+    inFile.read(inData, size);
+    inFile.close();
+    if (size < 0x30) {
+        cerr << "Input file is too small!" << endl;
+        return 1;
+    }
+
+    switch (*(u32*)inData) {
+        case PSP_MAGIC:
+            {
+                int outSize = pspDecryptPRX((const u8*)inData, (u8 *)outData, size, nullptr, true);
+                WriteFile(outFile.c_str(), outData, outSize);
+            }
+            break;
+        case PBP_MAGIC:
+            {
+                u32 pspOff = *(u32*)&inData[0x20];
+                u32 psarOff = *(u32*)&inData[0x24];
+                if (pspOff < size) {
+                    if (*(u32*)&inData[pspOff] == ELF_MAGIC) {
+                        cout << "Non-encrypted PSP file, writing to " << outFile << endl;
+                        WriteFile(outFile.c_str(), &inData[pspOff], psarOff - pspOff);
+                    } else {
+                        cout << "Decrypting PSP file to " << outFile << endl;
+                        int outSize = pspDecryptPRX((const u8 *)&inData[pspOff], (u8 *)outData, psarOff - pspOff, nullptr, true);
+                        WriteFile(outFile.c_str(), outData, outSize);
+                    }
+                }
+                if (psarOff < size) {
+                    cout << "Extracting PSAR to " << outDir << endl;
+                    pspDecryptPSAR((u8*)&inData[psarOff], (u32)size - psarOff, outDir);
+                }
+            }
+            break;
+        case PSAR_MAGIC:
+            pspDecryptPSAR((u8*)inData, size, outDir);
+            break;
+        case ELF_MAGIC:
+            cout << "Non-encrypted file, copying to " << outFile << endl;
+            WriteFile(outFile.c_str(), inData, size);
+            break;
+        default:
+            cout << "Unknown input file format!" << endl;
+            return 1;
+    }
+
+    return 0;
 }
